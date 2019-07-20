9b7a5bb60db9571eae70daf4f41fe87f92741ca0
monaelufabc <monael.ufabc@gmail.com>
2019-07-20 01:03:57 -0300

    ...

---
+++ BCC/Eco/Eco.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Eco
+{
+	public static void main(String[] args)
+	{
+		long n;
+		Scanner scan = new Scanner(System.in);
+
+		n = scan.nextLong();
+		System.out.println(n);
+	}
+}
---
+++ BCC/Nada/Nada.java
@@ -0,0 +1,6 @@
+public class Nada
+{
+	public static void main(String[] args)
+	{
+	}
+}
